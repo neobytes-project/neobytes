4d717c618f0f75730e3a774582d9e27bca0d8788
SikkieNL <SikkieNL@outlook.com>
2026-02-14 23:27:17 +0100

    Typo/doc updates and RPC help formatting

--- src/net.cpp
+++ src/net.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
 // Copyright (c) 2014-2017 The Dash Core developers
-// Copyright (c) 2021-2025 The Neobytes Core developers
+// Copyright (c) 2021-2026 The Neobytes Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
